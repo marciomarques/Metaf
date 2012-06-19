532433e3b0502afab5a1dd7949de5740b4209c19
Marcio Marques <marcio@caixadeideias.com.br>
2012-06-19 15:16:36 -0300

    Atualizacao de produtos

--- comerciais.php
+++ comerciais.php
@@ -1,32 +1,37 @@
-<a href="images/teste1-gr.png" data-link="http://google.com/" rel="lightbox[gallery1]">
-  <img src="images/teste1-tb.png" alt="<b>Projeto:</b> Nome | <b>Cliente</b> Nome" class="tb">
-</a>
-
-<a href="images/teste2-gr.png" rel="lightbox">
-  <img src="images/teste2-tb.png" alt="<b>Projeto:</b> Nome | <b>Cliente</b> Nome" class="tb">
-</a>
-
-
-<a href="images/teste3-gr.png" rel="lightbox[gallery1]">
-	<img src="images/teste3-tb.png" alt="<b>Projeto:</b> Nome | <b>Cliente</b> Nome" class="tb">
-</a>
-
-<a href="images/teste2-gr.png" rel="lightbox[gallery1]">
-	<img src="images/teste2-tb.png" alt="<b>Projeto:</b> Nome | <b>Cliente</b> Nome" class="tb">
-</a>
-
-<a href="images/teste1-gr.png" rel="lightbox[gallery1]">
-  <img src="images/teste1-tb.png" alt="<b>Projeto:</b> Nome | <b>Cliente</b> Nome" class="tb">
-</a>
-
-<a href="images/teste3-gr.png" rel="lightbox[gallery1]">
-	<img src="images/teste3-tb.png" alt="<b>Projeto:</b> Nome | <b>Cliente</b> Nome" class="tb">
-</a>
-
-<a href="images/teste2-gr.png" rel="lightbox[gallery1]">
-  <img src="images/teste2-tb.png" alt="<b>Projeto:</b> Nome | <b>Cliente</b> Nome" class="tb">
-</a>
-
-<a href="images/teste3-gr.png" rel="lightbox[gallery1]">
-	<img src="images/teste3-tb.png" alt="<b>Projeto:</b> Nome | <b>Cliente</b> Nome" class="tb">
-</a>
+	<ul id="horiz_container_outer">
+		<li id="horiz_container_inner">
+			<ul id="horiz_container">
+				<li style="float: left"><a href="images/comerciais/1.jpg"><img src="images/comerciais/1.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/2.jpg"><img src="images/comerciais/2.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/3.jpg"><img src="images/comerciais/3.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/4.jpg"><img src="images/comerciais/4.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/5.jpg"><img src="images/comerciais/5.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/6.jpg"><img src="images/comerciais/6.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/7.jpg"><img src="images/comerciais/7.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/8.jpg"><img src="images/comerciais/8.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/9.jpg"><img src="images/comerciais/9.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/10.jpg"><img src="images/comerciais/10.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/11.jpg"><img src="images/comerciais/11.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/12.jpg"><img src="images/comerciais/12.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/13.jpg"><img src="images/comerciais/13.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/14.jpg"><img src="images/comerciais/14.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/15.jpg"><img src="images/comerciais/15.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/16.jpg"><img src="images/comerciais/16.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/17.jpg"><img src="images/comerciais/17.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/18.jpg"><img src="images/comerciais/18.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/19.jpg"><img src="images/comerciais/19.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/20.jpg"><img src="images/comerciais/20.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/21.jpg"><img src="images/comerciais/21.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/comerciais/22.jpg"><img src="images/comerciais/22.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+			</ul>
+		</li>		
+	</ul>			
+	
+						
+	<div id="scrollbar">
+		<a id="left_scroll" class="mouseover_left" href="#"></a>
+		<div id="track">
+		     <div id="dragBar"></div>
+		</div>
+		<a id="right_scroll" class="mouseover_right" href="#"></a></div>
+	</div>	
--- visual.php
+++ visual.php
@@ -24,7 +24,38 @@
 				<li style="float: left"><a href="images/visual/21.jpg"><img src="images/visual/21.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
 				<li style="float: left"><a href="images/visual/22.jpg"><img src="images/visual/22.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
 				<li style="float: left"><a href="images/visual/23.jpg"><img src="images/visual/23.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
-				<li style="float: left"><a href="images/visual/24.jpg"><img src="images/visual/24.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/24.jpg"><img src="images/visual/24.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>			
+				<li style="float: left"><a href="images/visual/25.jpg"><img src="images/visual/25.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/26.jpg"><img src="images/visual/26.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/27.jpg"><img src="images/visual/27.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/28.jpg"><img src="images/visual/28.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/29.jpg"><img src="images/visual/29.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/30.jpg"><img src="images/visual/30.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/31.jpg"><img src="images/visual/31.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/32.jpg"><img src="images/visual/32.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/33.jpg"><img src="images/visual/33.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/34.jpg"><img src="images/visual/34.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/35.jpg"><img src="images/visual/35.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/36.jpg"><img src="images/visual/36.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/37.jpg"><img src="images/visual/37.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/38.jpg"><img src="images/visual/38.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/39.jpg"><img src="images/visual/39.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/40.jpg"><img src="images/visual/40.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/41.jpg"><img src="images/visual/41.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/42.jpg"><img src="images/visual/42.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/43.jpg"><img src="images/visual/43.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/44.jpg"><img src="images/visual/44.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/45.jpg"><img src="images/visual/45.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/46.jpg"><img src="images/visual/46.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/47.jpg"><img src="images/visual/47.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/48.jpg"><img src="images/visual/48.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/49.jpg"><img src="images/visual/49.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/50.jpg"><img src="images/visual/50.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/51.jpg"><img src="images/visual/51.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/52.jpg"><img src="images/visual/52.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/54.jpg"><img src="images/visual/54.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/55.jpg"><img src="images/visual/55.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>
+				<li style="float: left"><a href="images/visual/56.jpg"><img src="images/visual/56.jpg" width="600px" height="450px" alt="Nome do projeto" /></a></li>		
 			</ul>
 		</li>		
 	</ul>			
